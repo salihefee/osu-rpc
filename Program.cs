ed5ec97b9d2df831d5d42b2606dd61441ad89632
Salih Efe Ergür <salihefeergur1@gmail.com>
2023-09-06 13:55:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using DiscordRPC;
+using Newtonsoft.Json;
+
+namespace osu_rpc
+{
+    internal class Program
+    {
+        public static DiscordRpcClient? client;
+
+        static void Main(string[] args)
+        {
+            using (client = new DiscordRpcClient("1148786959167271044"))
+            {
+                client.OnReady += (sender, e) =>
+                {
+                    Console.WriteLine($"Received Ready from user {e.User.Username}");
+                };
+
+                Thread keyboardInterrupt = new Thread(() =>
+                {
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                });
+
+                keyboardInterrupt.Start();
+
+                client.Initialize();
+                while (true)
+                {
+                    client.SetPresence(new RichPresence()
+                    {
+                        Details = "",
+                        State = "Idle",
+                        Assets = new Assets()
+                        {
+                            LargeImageKey = "osu-logo",
+                            LargeImageText = "salihefee (rank #45,515)",
+                            SmallImageKey = "std-small",
+                            SmallImageText = "osu!"
+                        }
+                    });
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
